5c7328caa02e5afc17f77f10ad1839e8245f8588
Mamidi Saikrishna <151380948+thisisSaikrishna@users.noreply.github.com>
2025-10-17 23:27:56 +0530

    Add files via upload

---
+++ delete_product.php
@@ -0,0 +1,18 @@
+<?php
+include('../includes/connect.php');
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    // Get image name
+    $res = $conn->query("SELECT image FROM products WHERE id=$id");
+    $data = $res->fetch_assoc();
+
+    if ($data && file_exists("../assets/upload/" . $data['image'])) {
+        unlink("../assets/upload/" . $data['image']); // delete image file
+    }
+
+    $conn->query("DELETE FROM products WHERE id=$id");
+    echo "<script>alert('Product deleted successfully'); window.location='manage_products.php';</script>";
+}
+?>
